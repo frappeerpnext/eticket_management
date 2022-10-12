f5aff302220e60011caafd4ce4e37443f528b7d9
frappeerpnext <frapperpnext@gmail.com>
2022-10-12 12:45:14 +0700

    update eticket

--- eticket_management/e_ticket_management/doctype/ticket_booking/ticket_booking_calendar.js
+++ eticket_management/e_ticket_management/doctype/ticket_booking/ticket_booking_calendar.js
@@ -1,15 +1,22 @@
 frappe.views.calendar['Ticket Booking'] = {
+    name:"Ticket Booking Date",
     field_map: {
-        start: 'booking_date',
-        end: 'booking_date',
-        id: 'name',
-        allDay: 'all_day',
-        title: 'name',
-        color:"color"
+        "start": "booking_date",
+        "end": "booking_date",
+        "id": "name",
+        "allDay": "all_day",
+        "title": "name",
+        "color": "color",
+        "status":1
     },
-    style_map: {
-        Public: 'success',
-        Private: 'info'
-    },
+    gantt: true,
     order_by: 'name',
+    get_css_class: function (data) {
+        console.log(data);
+        if (data.is_checked_in == 1) {
+            return "success";
+        } else {
+            return "info"
+        }
+    }
 }
