02b3508235a713ecf15a8cc60b39e9dceab77379
frappeerpnext <frapperpnext@gmail.com>
2022-10-13 11:16:25 +0700

    update report

--- eticket_management/e_ticket_management/report/ticket_sold_report/ticket_sold_report.js
+++ eticket_management/e_ticket_management/report/ticket_sold_report/ticket_sold_report.js
@@ -1,44 +1,140 @@
-
-
+// Copyright (c) 2022, Frappe Technologies and contributors
+// For license information, please see license.txt
+/* eslint-disable */
 frappe.query_reports["Ticket Sold Report"] = {
 	"filters": [
 		{
-			fieldname: "start_date",
-			label: "Start Date",
-			fieldtype: "Date",
-			default:frappe.datetime.nowdate()
+			fieldname: "company",
+			label: "Company",
+			fieldtype: "Link",
+			options:"Company",
 		},
 		{
-			fieldname: "end_date",
-			label: "End Date",
-			fieldtype: "Date",
-			default:frappe.datetime.nowdate()
+			"fieldname":"filter_based_on",
+			"label": __("Filter Based On"),
+			"fieldtype": "Select",
+			"options": ["Fiscal Year", "Date Range"],
+			"default": ["Fiscal Year"],
+			"reqd": 1,
+			on_change: function() {
+				let filter_based_on = frappe.query_report.get_filter_value('filter_based_on');
+				frappe.query_report.toggle_filter_display('from_fiscal_year', filter_based_on === 'Date Range');
+				frappe.query_report.toggle_filter_display('start_date', filter_based_on === 'Fiscal Year');
+				frappe.query_report.toggle_filter_display('end_date', filter_based_on === 'Fiscal Year');
+
+				frappe.query_report.refresh();
+			}
+		},
+		{
+			"fieldname":"start_date",
+			"label": __("Start Date"),
+			"fieldtype": "Date",
+			default:frappe.datetime.get_today(),
+			"hidden": 1,
+			"reqd": 1
+		},
+		{
+			"fieldname":"end_date",
+			"label": __("End Date"),
+			"fieldtype": "Date",
+			default:frappe.datetime.get_today(),
+			"hidden": 1,
+			"reqd": 1
+		},
+		{
+			"fieldname":"from_fiscal_year",
+			"label": __("Start Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"default": frappe.defaults.get_user_default("fiscal_year"),
+			"reqd": 1,
+			on_change: () => {
+				frappe.model.with_doc("Fiscal Year", frappe.query_report.get_filter_value('from_fiscal_year'), function(r) {
+					let year_start_date = frappe.model.get_value("Fiscal Year", frappe.query_report.get_filter_value('from_fiscal_year'), "year_start_date");
+					frappe.query_report.set_filter_value({
+						period_start_date: year_start_date
+					});
+				});
+			}
+		},
+
+		{
+			"fieldname": "price_list",
+			"label": __("Sale Type"),
+			"fieldtype": "MultiSelectList",
+			get_data: function(txt) {
+				return frappe.db.get_link_options('Price List', txt,{"selling":1});
+			}
+		},
+		{
+			"fieldname": "customer_group",
+			"label": __("Customer Group"),
+			"fieldtype": "MultiSelectList",
+			get_data: function(txt) {
+				
+				return frappe.db.get_link_options('Customer Group', txt,{"is_group":0});
+			}
+		},
+		{
+			fieldname: "market_segment",
+			label: "Market Segment",
+			"fieldtype": "MultiSelectList",
+			get_data: function(txt) {
+				return frappe.db.get_link_options('Market Segment', txt);
+			}
+		},
+		{
+			fieldname: "business_source",
+			label: "Business Source",
+			"fieldtype": "MultiSelectList",
+			get_data: function(txt) {
+				return frappe.db.get_link_options('Lead Source', txt);
+			}
 		},
 		{
 			"fieldname": "parent_row_group",
-			"label": __("Parent Row Group"),
+			"label": __("Parent Group By"),
 			"fieldtype": "Select",
-			"options": "None\nItem\nSale Invoice\nCashier",
-			"default":"None"
+			"options": "\nCategory\nProduct Group\nBrand\nCompany\nPrice List\nCustomer\nCustomer Group\nMembership\nTerritory\nDate\n\Month\nYear\nSale Invoice\nMarket Segment\nMarketing Segment Type\nBusiness Source\nBusiness Source Type",
+			
 		},
 		{
 			"fieldname": "row_group",
-			"label": __("Row Group"),
+			"label": __("Row Group By"),
 			"fieldtype": "Select",
-			"options": "Item\nSale Invoice\nCashier",
-			"default":"Item"
+			"options": "Product\nCategory\nProduct Group\nBrand\nCompany\nPrice List\nCustomer\nCustomer Group\nMembership\nTerritory\nDate\n\Month\nYear\nSale Invoice\nMarket Segment\nMarketing Segment Type\nBusiness Source\nBusiness Source Type",
+			"default":"Category"
 		},
 		{
 			"fieldname": "column_group",
-			"label": __("Column Group By"),	
+			"label": __("Column Group By"),
 			"fieldtype": "Select",
-			"options": "None\nDaily\nMonthly\nYearly",
-			"default":"None"
+			"options": "Column Group By\nDaily\nWeekly\nMonthly\nQuarterly\nHalf Yearly\nYearly",
+			"default":"Column Group By"
 		},
 		{
-			fieldname: "is_master_ticket",
-			label: "Show Master Ticket",
-			fieldtype: "Check"
+			"fieldname": "hide_columns",
+			"label": __("Hide Columns"),
+			"fieldtype": "MultiSelectList",
+			get_data: function(txt) {
+				return [
+					{"value":"Amount","description":"Amount"},
+					{"value":"Quantity","description":"Quantity"},
+					{"value":"Sub Total","description":"Sub Total"},
+					 
+					{"value":"Total Discount","description":"Total Discount"},
+				]
+			},
+			"default":"All"
 		},
-	]
+		{
+			"fieldname": "chart_type",
+			"label": __("Chart Type"),
+			"fieldtype": "Select",
+			"options": "None\nbar\nline\npie",
+			"default":"bar"
+		}
+	],
 };
+
+ 
