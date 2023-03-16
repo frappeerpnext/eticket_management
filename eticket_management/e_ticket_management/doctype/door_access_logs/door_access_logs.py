62ddcc3ed23069dfe630d560ed2e0d688e614485
frappeerpnext <frapperpnext@gmail.com>
2023-03-16 10:11:11 +0000

    2023-03-16 5:11 PM

--- eticket_management/e_ticket_management/doctype/door_access_logs/door_access_logs.py
+++ eticket_management/e_ticket_management/doctype/door_access_logs/door_access_logs.py
@@ -26,52 +26,28 @@ class DoorAccessLogs(Document):
 			
 			frappe.db.sql("update `tabPOS Ticket` set checked_in_time=%s, is_checked_in=1 where is_checked_in = 0 and ticket_number =%s",	(self.transaction_date,self.card_number))
 			frappe.db.commit()
+   
 		#update ticket total check in history
-	 
 		if self.status == "Success":
 			if frappe.db.exists("Ticket Code",self.card_number, cache=True):
 				doc = frappe.get_doc("Ticket Code",self.card_number)
 				if doc:
-					
 					doc.total_checked_in = (doc.total_checked_in or 0) + 1
 					doc.last_checked_in_date = self.transaction_date
 					doc.save()
 					frappe.db.commit()
 			#generate Door Access Logs From Master Ticket
 			if self.is_master:
+				frappe.msgprint("eticket_management.e_ticket_management.doctype.door_access_logs.door_access_logs.generate_door_access_log_from_master")
 				frappe.enqueue('eticket_management.e_ticket_management.doctype.door_access_logs.door_access_logs.generate_door_access_log_from_master', queue='long',self=self)
 			
-
+	# def before_save(self):
+	# 	if self.is_master:
+	# 		frappe.msgprint('hello')
+	# 		frappe.enqueue('eticket_management.e_ticket_management.doctype.door_access_logs.door_access_logs.generate_door_access_log_from_master', queue='long',self=self)
 def generate_door_access_log_from_master(self):
 	#Generate from POS
-	if self.pos_invoice_id :
-		sql = """SELECT 
-					ticket_number FROM `tabPOS Ticket` a 
-				WHERE a.pos_invoice_id = '{0}' AND 
-					a.is_master_ticket_number = 0 AND 
-					a.ticket_number NOT IN 
-						(
-							SELECT 
-								card_number 
-							FROM `tabDoor Access Logs` b 
-							WHERE 
-								b.pos_invoice_id = '{0}'
-						)""".format(self.pos_invoice_id)
-		pos_tickets = frappe.db.sql(sql, as_dict=1)
-		for t in pos_tickets:
-			doc = frappe.new_doc("Door Access Logs")
-			doc.pos_invoice_id = self.pos_invoice_id
-			doc.posting_date = self.posting_date
-			doc.door_name = self.door_name
-			doc.direction = self.direction
-			doc.ip_address = self.ip_address
-			doc.transaction_date = self.transaction_date
-			doc.status = self.status
-			doc.is_master = 0
-			doc.card_number = t.ticket_number 
-			doc.save()
-		frappe.db.commit()
-	else:
+	if self.booking_number :
 		sql = """SELECT 
   					a.ticket_number 
        			FROM `tabBooking Active Ticket` a 
@@ -101,3 +77,31 @@ def generate_door_access_log_from_master(self):
 			doc.card_number = t.ticket_number
 			doc.save()
 		frappe.db.commit()
+	else:
+		sql = """SELECT 
+					ticket_number FROM `tabPOS Ticket` a 
+				WHERE a.pos_invoice_id = '{0}' AND 
+					a.is_master_ticket_number = 0 AND 
+					a.ticket_number NOT IN 
+						(
+							SELECT 
+								card_number 
+							FROM `tabDoor Access Logs` b 
+							WHERE 
+								b.pos_invoice_id = '{0}'
+						)""".format(self.pos_invoice_id)
+		pos_tickets = frappe.db.sql(sql, as_dict=1)
+		for t in pos_tickets:
+			doc = frappe.new_doc("Door Access Logs")
+			doc.pos_invoice_id = self.pos_invoice_id
+			doc.posting_date = self.posting_date
+			doc.door_name = self.door_name
+			doc.direction = self.direction
+			doc.ip_address = self.ip_address
+			doc.transaction_date = self.transaction_date
+			doc.status = self.status
+			doc.is_master = 0
+			doc.card_number = t.ticket_number 
+			doc.save()
+		frappe.db.commit()
+		
