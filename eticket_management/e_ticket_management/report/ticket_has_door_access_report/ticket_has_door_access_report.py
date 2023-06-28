3998cbdebfa8c82b6aa8d6ad73833ae6aae3b133
frappeerpnext <frapperpnext@gmail.com>
2023-06-28 09:52:02 +0000

    update 2023-06-28 4:51 PM

--- eticket_management/e_ticket_management/report/ticket_has_door_access_report/ticket_has_door_access_report.py
+++ eticket_management/e_ticket_management/report/ticket_has_door_access_report/ticket_has_door_access_report.py
@@ -24,7 +24,7 @@ def get_data(filters):
 				ifnull(a.pos_document_number,c.document_number) as reference,
 				coalesce(b.posting_date,'No Door Access') posting_date
 			from `tabPOS Ticket` a
-			inner join `tabDoor Access Logs` b on b.card_number = a.ticket_number
+			left join `tabDoor Access Logs` b on b.card_number = a.ticket_number
 			inner join `tabPOS Invoice` c on c.name = a.pos_invoice
 			where a.transaction_date between '{0}' and '{1}'
 			group by a.ticket_number
@@ -35,7 +35,7 @@ def get_data(filters):
 				a.name as reference,
 				coalesce(posting_date,'No Door Access') posting_date
 			from `tabTicket Booking` a 
-			inner join `tabBooking Active Ticket` b on a.name = b.parent
+			left join `tabBooking Active Ticket` b on a.name = b.parent
 			inner join `tabDoor Access Logs` c on c.card_number = b.ticket_number
 			where b.arrival_date between '{0}' and '{1}'
 			group by  b.ticket_number
